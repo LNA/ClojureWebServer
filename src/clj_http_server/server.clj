e5af98fb49c88b0f753ce7d07e46711b6923ed9b
LNA <LAllen@8thLight.com>
2015-02-27 18:57:37 -0600

    Gives a ServerIO function for reading & writing

--- src/clj_http_server/server.clj
+++ src/clj_http_server/server.clj
@@ -1,7 +1,21 @@
 (ns clj_http_server.server
   (:import (java.net ServerSocket SocketException)
-                      (java.io BufferedReader PrintWriter InputStreamReader StringWriter))
+           (java.io BufferedReader DataOutputStream PrintWriter InputStreamReader StringWriter))
   (:require [clojure.java.io :as io]))
+
+(defn writer [open-socket]
+  (let [writer (DataOutputStream. (.getOutputStream open-socket))]
+    writer))
+
+(defn reader [open-socket]
+  (let [reader (BufferedReader. (InputStreamReader. (.getInputStream open-socket)))]
+    reader))
+
+(defn server-io [server-socket]
+  (let [open-socket (.accept server-socket)
+        reader (reader open-socket)
+        writer (writer open-socket)]
+  (prn "Accepted connection, created reader and writer.")))
 
 (defn create-socket [port]
   (ServerSocket. port))
@@ -9,5 +23,7 @@
 (defn start [port]
   (let [server-socket (create-socket port)]
     (while (not (.isClosed server-socket))
-  (prn "Starting the server")
-  )))
+      (try
+        (prn "Starting the server")
+        (server-io server-socket)
+        (catch Exception e)))))
